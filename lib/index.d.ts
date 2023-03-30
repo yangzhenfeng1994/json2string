20208477f71d8b9919141c87e30a2407f71ab36b
yzf <yangzhenfeng1994@gmail.com>
2023-03-30 17:29:20 +0800

    Update index.d.ts

--- lib/index.d.ts
+++ lib/index.d.ts
@@ -2,6 +2,6 @@ interface Iobject {
     [name: string | number | symbol]: any;
 }
 declare const stringify: (obj: Iobject) => string;
-declare const parse: (str: string, date2obj?: boolean) => {};
-declare const clone: (obj: Iobject, date2obj?: boolean) => {};
+declare const parse: (str: string, date2obj?: boolean) => Iobject;
+declare const clone: (obj: T, date2obj?: boolean) => T;
 export { stringify, parse, clone };
